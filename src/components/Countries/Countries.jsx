3bb3d54338e03f2b2b50682b88f5de7d3172aa40
jb <joyassroy@Joyassroys-MacBook-Air.local>
2025-09-19 15:01:22 +0600

    all done practice work

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,32 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country'
 import './Countries.css'
 
 const Countries = ({countrriesPromise}) => {
+    const [count,setCount]=useState([]);
     const countriesData=use(countrriesPromise);
     const countries=countriesData.countries;
-    console.log(countries);
+    const handleVisitedCountries=(cu)=>{
+        // setCount(cu);
+        if(count.find(cou=>(cou==cu))){
+            let newVisited=[...count];
+            newVisited=newVisited.filter(item => item !== cu);
+            setCount(newVisited);
+        }
+        else {
+        const newVisitedCountries=[...count,cu];
+        setCount(newVisitedCountries);
+        }
+
+        // console.log(cu)
+    }
     return (
         <div>
             <h2>Countires are {countries.length}</h2>
+            <h2>Visited Countries Are {count.length}</h2>
             <div className='countries'>
                 {
-                countries.map(country=><Country key={country.cca3.cca3} country={country}></Country>)
+                countries.map(country=><Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
             }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country,handleVisitedCountries }) => {
     // console.log(country.name.common)
     const [count, setCount] = useState(0);
     const clickHandle = () => {
         const cu = count + 1;
         setCount(cu);
-
+        handleVisitedCountries(country);
 
     }
     return (
-        <div className='country'>
+        <div className={`country ${(count%2) && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Area: {country.area.area} {country.area.area > 30000 ? "Big Country" : "Small Country"}</p>
